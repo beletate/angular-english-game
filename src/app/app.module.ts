d25f6d4ec7a2f7112c8cd45f39ecb4c2266c9ead
beletate <vinicius_beletate@hotmail.com>
2021-03-28 21:37:44 -0300

    Create menu page and choosing difficulty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MedioComponent } from './medio/medio.component';
+import { DificilComponent } from './dificil/dificil.component';
+import { FacilComponent } from './facil/facil.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FacilComponent,
+    MedioComponent,
+    DificilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
