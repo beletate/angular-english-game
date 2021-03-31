6a773054c2d1af6dad07365f6aa31168c6f812e3
beletate <vinicius_beletate@hotmail.com>
2021-03-31 16:08:00 -0300

    Adicionando perguntas dificies

--- src/app/dificil/dificil.component.ts
+++ src/app/dificil/dificil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-dificil',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DificilComponent implements OnInit {
 
+  dificilMenu: FormGroup
+  count = 1
+  acertos = 0
+  mensagem: string
+  respostasCorretas = ['a', 'd', 'b', 'c', 'b']
+  respostasJogador = []
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  escolherOpcao(value: string) {
+    this.count = this.count + 1
+    this.respostasJogador.push(value)
+    if (this.count == 6) {
+      this.checarRespostas()
+      this.mensagem = this.acertos >= 3 ? "Parabéns, vejo que se dedicou muito ao teste! Vá para o próximo nível!" : "Não desista, continue tentando!"
+    }
+  }
+  checarRespostas() {
+    for (let i in this.respostasCorretas) {
+      if (this.respostasJogador[i] === this.respostasCorretas[i]) {
+        this.acertos = this.acertos + 1
+      }
+    }
+  }
 }
